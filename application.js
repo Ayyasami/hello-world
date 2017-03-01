32dbe67d3644aea4a866355bddc20e8cd2929fe2
ays <ayyasamit@gmail.com>
2017-03-01 11:01:11 +0530

    added new table

---
+++ application.js
@@ -0,0 +1,23 @@
+<body>
+
+<h1>Famous Cities</h1>
+
+<h2>Tokyo</h2>
+<p>Tokyo is the capital of Japan, the center of the Greater Tokyo Area,
+and the most populous metropolitan area in the world.
+It is the seat of the Japanese government and the Imperial Palace,
+and the home of the Japanese Imperial Family.</p>
+
+</body>
+  <tr>
+    <th>Name</th>
+    <th>Description</th </tr>
+  <tr>
+    <td>A</td>
+    <td>Description of A</td>
+  </tr>
+  <tr>
+    <td>B</td>
+    <td>Description of B</td>
+  </tr>
+</table>
